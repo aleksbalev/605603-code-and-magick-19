4fe1ed0c9d7b8b15786a738c542ddb3a66a1e7a2
AleksBalev <aleks.balev7899@gmail.com>
2020-02-08 11:22:16 +0100

    Написал код для перемещения диалога и пердмета в нем, также разделил код на отдельные модули

---
+++ js/setup-wizard.js
@@ -0,0 +1,41 @@
+'use strict';
+
+(function () {
+  var EYES_COLORS = window.utils.EYES_COLORS;
+  var COAT_COLORS = window.utils.COAT_COLORS;
+  var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+  var getRandom = window.utils.getRandom;
+
+  /* Селекторы для настройки цветов по нажатию на волшебника */
+  var setupPlayer = document.querySelector('.setup-player');
+  var setupWizard = document.querySelector('.setup-wizard');
+  var wizardCoat = setupWizard.querySelector('.wizard-coat');
+  var wizardEyes = setupWizard.querySelector('.wizard-eyes');
+  var wizardFireball = document.querySelector('.setup-fireball-wrap');
+
+  var attributeCoat = setupPlayer.querySelector('[name=\'coat-color\']');
+  var attributeEyes = setupPlayer.querySelector('[name=\'eyes-color\']');
+  var attributeFireball = wizardFireball.querySelector('[name=\'fireball-color\']');
+  /* Селекторы для настройки цветов по нажатию на волшебника */
+
+  /* Обработчики событий по изменению настроек мага (цвет - глаз, мантии, фаербола) */
+  wizardCoat.addEventListener('click', function () {
+    var randomColor = getRandom(COAT_COLORS);
+    wizardCoat.style.fill = randomColor;
+    attributeCoat.value = randomColor;
+  });
+
+  wizardEyes.addEventListener('click', function () {
+    var randomColor = getRandom(EYES_COLORS);
+    wizardEyes.style.fill = randomColor;
+    attributeEyes.value = randomColor;
+  });
+
+  wizardFireball.addEventListener('click', function () {
+    var randomColor = getRandom(FIREBALL_COLORS);
+    wizardFireball.style.backgroundColor = randomColor;
+    attributeFireball.value = randomColor;
+  });
+  /* Обработчики событий по изменению настроек мага (цвет - глаз, мантии, фаербола) */
+})();
---
+++ js/utils.js
@@ -0,0 +1,28 @@
+'use strict';
+
+(function () {
+  var ESC_KEY = 'Escape';
+  var ENTER_KEY = 'Enter';
+
+  var COAT_COLORS = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ];
+  var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+  var getRandom = function (list) {
+    return list[Math.floor(Math.random() * list.length)];
+  };
+
+  window.utils = {
+    ESC_KEY: ESC_KEY,
+    ENTER_KEY: ENTER_KEY,
+    COAT_COLORS: COAT_COLORS,
+    EYES_COLORS: EYES_COLORS,
+    getRandom: getRandom
+  };
+})();
